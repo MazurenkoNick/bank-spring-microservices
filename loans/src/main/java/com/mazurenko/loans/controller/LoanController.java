104ac7b30d894bed1db2adcfbe58ad6fcf365911
nikita <nikitamazurenko42@gmail.com>
2023-04-05 20:28:09 +0300

    update loans service to read from config server

--- loans/src/main/java/com/mazurenko/loans/controller/LoanController.java
+++ loans/src/main/java/com/mazurenko/loans/controller/LoanController.java
@@ -4,6 +4,7 @@ import com.mazurenko.loans.model.Customer;
 import com.mazurenko.loans.model.Loan;
 import com.mazurenko.loans.repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,8 +15,12 @@ public class LoanController {
     @Autowired
     private LoanRepository loanRepository;
 
+    @Value("${loans.message}")
+    private String message;
+
     @PostMapping("/myLoans")
     public List<Loan> getLoanDetails(@RequestBody Customer customerDto) {
+        System.out.println(message);
         return loanRepository.findByCustomerIdOrderByStartDtDesc(customerDto.getCustomerId());
     }
 }
