a39d44735615b4f840c5aecc0a403fdee26571f1
nikita <nikitamazurenko42@gmail.com>
2023-04-05 19:01:08 +0300

    update accounts microservice to read properties from config server

---
+++ accounts/src/main/java/com/mazurenko/accounts/controller/ConfigPropertiesController.java
@@ -0,0 +1,27 @@
+package com.mazurenko.accounts.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.mazurenko.accounts.config.AccountsServiceConfig;
+import com.mazurenko.accounts.model.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/properties")
+public class ConfigPropertiesController {
+
+    @Autowired
+    private AccountsServiceConfig accountsServiceConfig;
+
+    @GetMapping("/details/")
+    private String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsServiceConfig);
+        String jsonString = ow.writeValueAsString(properties);
+        return jsonString;
+    }
+}
---
+++ accounts/src/main/java/com/mazurenko/accounts/model/Properties.java
@@ -0,0 +1,31 @@
+package com.mazurenko.accounts.model;
+
+import com.mazurenko.accounts.config.AccountsServiceConfig;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter @Setter
+public class Properties {
+
+    private String msg;
+    private String buildVersion;
+    private Map<String, String> mailDetails;
+    private List<String> activeBranches;
+
+    public Properties(String msg, String buildVersion, Map<String, String> mailDetails, List<String> activeBranches) {
+        this.msg = msg;
+        this.buildVersion = buildVersion;
+        this.mailDetails = mailDetails;
+        this.activeBranches = activeBranches;
+    }
+
+    public Properties(AccountsServiceConfig accountsServiceConfig) {
+        this.msg = accountsServiceConfig.getMsg();
+        this.buildVersion = accountsServiceConfig.getBuildVersion();
+        this.mailDetails = accountsServiceConfig.getMailDetails();
+        this.activeBranches = accountsServiceConfig.getActiveBranches();
+    }
+}
